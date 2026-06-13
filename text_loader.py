4804db62214f3a3e5b297a6e5beeb1cf3bbafe6a
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-14 01:12:10 +0530

    basics of txt document loader implemented

---
+++ text_loader.py
@@ -0,0 +1,11 @@
+from langchain_community.document_loaders import TextLoader
+
+loader = TextLoader("cricket.txt", encoding="utf-8")
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(type(docs[0]))
