90f955dc41ade4fd6ff07b927f780f7d1378aa48
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-14 01:30:46 +0530

    text loader completed

--- text_loader.py
+++ text_loader.py
@@ -1,4 +1,20 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template="Write a summary for the following poem {poem}", input_variables=["poem"]
+)
+
+parser = StrOutputParser()
 
 loader = TextLoader("cricket.txt", encoding="utf-8")
 
@@ -9,3 +25,13 @@ print(type(docs))
 print(len(docs))
 
 print(type(docs[0]))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+result = chain.invoke({"poem": docs[0].page_content})
+
+print(result)
