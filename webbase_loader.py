f5659de78a63ede164d3f0ffd19d1479e9d7eaca
Avinash Singh Rathore <avinash@Avinashs-MacBook-Air.local>
2026-06-14 23:34:10 +0530

    implemented how to ask question on webase loader content

--- webbase_loader.py
+++ webbase_loader.py
@@ -1,5 +1,20 @@
 from langchain_community.document_loaders import WebBaseLoader
 from bs4 import BeautifulSoup
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(repo_id="deepseek-ai/DeepSeek-V4-Pro", task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+prompt = PromptTemplate(
+    template="Answer the following question \n {question} from the following text \n {text}",
+    input_variables=["question", "text"],
+)
 
 url = "https://www.myntra.com/shop/men"
 
@@ -7,6 +22,16 @@ loader = WebBaseLoader(url)
 
 docs = loader.load()
 
-print(len(docs))
+parser = StrOutputParser()
 
-print(docs[0].page_content)
+chain = prompt | model | parser
+
+result = chain.invoke(
+    {"question": "What is the product we are asking", "text": docs[0].page_content}
+)
+
+print(result)
+
+# print(len(docs))
+
+# print(docs[0].page_content)
